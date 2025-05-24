d55e91d4ab8aaca1cb23c3687098ab0aede70dc5
Sowmik Roy <srshowmik2001@gmail.com>
2025-05-25 03:25:13 +0600

    doctors update

--- MediConsult-frontend/frontend/src/assets/assets.js
+++ MediConsult-frontend/frontend/src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_pic from './profile_pic.png'
 import contact_image from './contact_image.png'
 import about_image from './about_image.png'
 import logo from './logo.svg'
+import logo2 from './logo2.png'
 import dropdown_icon from './dropdown_icon.svg'
 import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.png'
@@ -43,6 +44,7 @@ export const assets = {
     header_img,
     group_profiles,
     logo,
+    logo2,
     chats_icon,
     verified_icon,
     info_icon,
--- MediConsult-frontend/frontend/src/components/Footer.jsx
+++ MediConsult-frontend/frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         {/* ----------------left--------- */}
 
         <div>
-          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <img className='mb-5 w-40' src={assets.logo2} alt="" />
           <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
 
