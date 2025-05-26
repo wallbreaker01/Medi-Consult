70ffb772e934c6b80bf38f28af930e1d007576f7
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2025-05-26 17:16:37 +0600

    initial server setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running on localhost:4000');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
