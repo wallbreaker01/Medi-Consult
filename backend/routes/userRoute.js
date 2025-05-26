1a7ccf39c85b51713ddfa8bb60fc7e30bac7db99
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2025-05-27 03:01:23 +0600

    get profile & update-profile api created

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { loginUser, registerUser, getProfile, updateProfile } from '../controllers/userController.js';
+import upload from '../middleware/multer.js';
+import authUser from '../middleware/authUser.js';
+
 
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser)
 userRouter.post("/login", loginUser)
+userRouter.get("/get-profile", authUser, getProfile)
+userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile)
 
 export default userRouter;
