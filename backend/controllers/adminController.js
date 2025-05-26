502873ecbfc221ce6c54bb33627f71d429f58497
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2025-05-27 03:51:26 +0600

    add-doctor & all doctors api created

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,9 @@
 import jwt from "jsonwebtoken";
+import doctorModel from "../models/doctorModel.js";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import { v2 as cloudinary } from "cloudinary";
+
 
 // API for admin login
 const loginAdmin = async (req, res) => {
@@ -25,7 +30,100 @@ const loginAdmin = async (req, res) => {
   }
 };
 
+// API for adding Doctor
+const addDoctor = async (req, res) => {
+    try {
+      const {
+        name,
+        email,
+        password,
+        speciality,
+        degree,
+        experience,
+        about,
+        fees,
+        address,
+      } = req.body;
+      const imageFile = req.file;
+  
+      
+      if (
+        !name ||
+        !email ||
+        !password ||
+        !speciality ||
+        !degree ||
+        !experience ||
+        !about ||
+        !fees ||
+        !address
+      ) {
+        return res.json({ success: false, message: "Missing Details" });
+      }
+  
+      
+      if (!validator.isEmail(email)) {
+        return res.json({
+          success: false,
+          message: "Please enter a valid email",
+        });
+      }
+  
+      
+      if (password.length < 5) {
+        return res.json({
+          success: false,
+          message: "Please enter a strong password",
+        });
+      }
+  
+      
+      const salt = await bcrypt.genSalt(10); // the more no. round the more time it will take
+      const hashedPassword = await bcrypt.hash(password, salt);
+  
+      
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      });
+      const imageUrl = imageUpload.secure_url;
+  
+      const doctorData = {
+        name,
+        email,
+        image: imageUrl,
+        password: hashedPassword,
+        speciality,
+        degree,
+        experience,
+        about,
+        fees,
+        address: JSON.parse(address),
+        date: Date.now(),
+      };
+  
+      const newDoctor = new doctorModel(doctorData);
+      await newDoctor.save();
+      res.json({ success: true, message: "Doctor Added" });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: error.message });
+    }
+  };
+  
+  // API to get all doctors list for admin panel
+  const allDoctors = async (req, res) => {
+    try {
+      const doctors = await doctorModel.find({}).select("-password");
+      res.json({ success: true, doctors });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: error.message });
+    }
+  };
+
 export {
-    loginAdmin
+    loginAdmin,
+    addDoctor,
+    allDoctors
   };
   
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { loginAdmin } from '../controllers/adminController.js';
+import { loginAdmin,addDoctor,allDoctors } from '../controllers/adminController.js';
+import authAdmin from '../middleware/authAdmin.js';
+import upload from '../middleware/multer.js';
 const adminRouter = express.Router();
 
 adminRouter.post("/login", loginAdmin)
+adminRouter.post("/add-doctor", authAdmin, upload.single('image'), addDoctor)
+adminRouter.get("/all-doctors", authAdmin, allDoctors)
 
 export default adminRouter;
