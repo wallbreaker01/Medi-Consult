e505681af56148402669768d5928a1ac6c34c589
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2025-05-27 01:42:51 +0600

    register and login api created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from 'cors'
 import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js"
 import dotenv from 'dotenv';
 
 
@@ -11,11 +13,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 
 // middlewares
 app.use(express.json())
 app.use(cors())
+
+// api endpoints
+app.use("/api/user", userRouter)
 
 
 
