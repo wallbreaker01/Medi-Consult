a8803afa7fe587f64120eca9965b3800dccce910
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2025-05-30 03:36:34 +0600

    context and login page created for admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Login from './pages/Login'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
-      
+      <Login/>
+      <ToastContainer/>
     </div>
   )
 }
