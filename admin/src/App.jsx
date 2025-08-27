25a4ab863a9a7004619d9c4079f6012d6346a533
Sowmik Roy <srshowmik2001@gmail.com>
2025-08-27 19:19:44 +0600

    minor change

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
-          <Route path='/' element={<></>} />
+          <Route path='/' element={<Dashboard/>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllAppointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
